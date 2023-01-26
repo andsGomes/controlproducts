8b520e79683ea2e42f5dd4a6b111cd2fcf2c7624
andsGomes <anderson.stos.gomes@gmail.com>
2023-01-26 10:08:24 -0300

    First Commit

---
+++ Entities/Order.cs
@@ -0,0 +1,17 @@
+using osenums.Entities.Enums;
+
+namespace osenums.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return @"Id:{Id}, Data:{Moment}, Status:{Status}";
+        }
+        
+    }
+}
